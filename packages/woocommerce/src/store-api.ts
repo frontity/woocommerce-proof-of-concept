10dfe4e67ef8f754a17b083fdc3cab2db67d7d60
David Arenas <darensr@gmail.com>
2020-12-16 15:00:02 +0100

    Document `storeApi`

--- packages/woocommerce/src/store-api.ts
+++ packages/woocommerce/src/store-api.ts
@@ -1,14 +1,52 @@
 import { State } from "frontity/types";
 import { Packages } from "../types";
 
+/**
+ * Params passed to {@link storeApi}.
+ */
 type StoreApiPayload = {
+  /**
+   * The Frontity state.
+   */
   state: State<Packages>;
+
+  /**
+   * Specific endpoint of the `/wc/store/` API, without the initial slash.
+   *
+   * @example "cart"
+   * @example "cart/add-item"
+   */
   endpoint: string;
+
+  /**
+   * The HTTP method used to send the request.
+   *
+   * @defaultValue `"GET"`
+   */
   method?: "GET" | "POST" | "PUT";
+
+  /**
+   * Map of search params used in this request.
+   *
+   * @defaultValue `{}`
+   */
   params?: Record<string, any>;
+
+  /**
+   * Serializable object that will be sent in JSON format.
+   *
+   * @defaultValue `undefined`
+   */
   body?: unknown;
 };
 
+/**
+ * Send requests to the `/wc/store/` API added by the WooCommerce Blocks plugin.
+ *
+ * @param payload - Object of type {@link StoreApiPayload}.
+ *
+ * @returns A promise that resolves with the returned value.
+ */
 const storeApi = async <Result = unknown>({
   state,
   endpoint,
@@ -30,7 +68,6 @@ const storeApi = async <Result = unknown>({
   );
 
   const result: Result = await response.json();
-
   return result;
 };
 
