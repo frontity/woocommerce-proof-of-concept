14aa9c77002c8728d9bd222c1057a0ea68cd3cec
David Arenas <darensr@gmail.com>
2020-12-16 10:07:19 +0100

    Update Checkout component to use BillingField

--- packages/woocommerce-theme/src/components/woocommerce/checkout.tsx
+++ packages/woocommerce-theme/src/components/woocommerce/checkout.tsx
@@ -11,7 +11,7 @@ import BillingField from "./billing-field";
  */
 const Checkout: React.FC = () => {
   // Get the frontity state.
-  const { state, actions } = useConnect<Packages>();
+  const { state } = useConnect<Packages>();
   const { cart } = state.woocommerce;
 
   // If there's no cart yet, just return null.
@@ -26,17 +26,6 @@ const Checkout: React.FC = () => {
       </Container>
     );
 
-  // Get the data of the product.
-  const { billing_address } = cart;
-
-  const updateBillingField = (field: keyof BillingAddress) => (e) => {
-    actions.woocommerce.updateCustomer({
-      billingAddress: {
-        [field]: e.target.value,
-      },
-    });
-  };
-
   return (
     <Container>
       <Summary>Summary</Summary>
@@ -44,41 +33,17 @@ const Checkout: React.FC = () => {
         <h2>Contact information</h2>
         <FormSection>
           <BillingField label="Email address" field="email" />
-          <Field>
-            <label>Phone number</label>
-            <input type="text" value={billing_address.phone} />
-          </Field>
+          <BillingField label="Phone number" field="phone" />
         </FormSection>
         <h2>Billing Address</h2>
         <FormSection>
-          <Field>
-            <label>First name</label>
-            <input type="text" value={billing_address.first_name} />
-          </Field>
-          <Field>
-            <label>Last name</label>
-            <input type="text" value={billing_address.last_name} />
-          </Field>
-          <Field>
-            <label>Address</label>
-            <input type="text" value={billing_address.address_1} />
-          </Field>
-          <Field>
-            <label>Address (2nd line)</label>
-            <input type="text" value={billing_address.address_2} />
-          </Field>
-          <Field>
-            <label>City</label>
-            <input type="text" value={billing_address.city} />
-          </Field>
-          <Field>
-            <label>State</label>
-            <input type="text" value={billing_address.state} />
-          </Field>
-          <Field>
-            <label>Postcode</label>
-            <input type="text" value={billing_address.postcode} />
-          </Field>
+          <BillingField label="First name" field="first_name" />
+          <BillingField label="Last name" field="last_name" />
+          <BillingField label="Address" field="address_1" />
+          <BillingField label="Adderss (2nd line)" field="address_2" />
+          <BillingField label="City" field="city" />
+          <BillingField label="State" field="state" />
+          <BillingField label="Postcode" field="postcode" />
         </FormSection>
         <h2>Payment method</h2>
         <p>Only check payments are allowed right now</p>
@@ -106,32 +71,6 @@ const Summary = styled.div``;
 const Form = styled.div``;
 
 const FormSection = styled.div``;
-
-const Field = styled.div`
-  label {
-    display: block;
-    padding: 2px 0;
-    color: #444;
-    font-size: 0.8em;
-  }
-
-  input,
-  textarea {
-    border-style: solid;
-    border-width: 1px;
-    width: 95%;
-    padding: 0 8px;
-    line-height: 2;
-    min-height: 30px;
-    box-shadow: 0 0 0 transparent;
-    border-radius: 4px;
-    border: 1px solid #7e8993;
-    background-color: #fff;
-    color: #32373c;
-    margin: 0 1px;
-    font-size: 1.2em;
-  }
-`;
 
 const ShopMessage = styled.h2`
   text-align: center;
