b4dbb0ae9f0e66ee804812cb7cb7f19e6229a0ac
Luis Herranz <luisherranz@gmail.com>
2020-12-22 09:47:46 +0100

    Remove unnecessary batches

--- packages/woocommerce/src/index.ts
+++ packages/woocommerce/src/index.ts
@@ -1,4 +1,3 @@
-import { batch } from "frontity";
 import merge from "lodash.merge";
 import WooCommerce, { Checkout } from "../types";
 import ProductHandler from "./handlers/product";
@@ -151,10 +150,8 @@ const wooCommerce: WooCommerce = {
       }) => {
         // First, update the current state. This would be overwritten with the
         // REST API response but we can show the changes in advance.
-        batch(() => {
-          merge(state.woocommerce.cart.billing_address, billingAddress);
-          merge(state.woocommerce.cart.shipping_address, shippingAddress);
-        });
+        merge(state.woocommerce.cart.billing_address, billingAddress);
+        merge(state.woocommerce.cart.shipping_address, shippingAddress);
 
         // Get updated values from the state.
         const { billing_address, shipping_address } = state.woocommerce.cart;
